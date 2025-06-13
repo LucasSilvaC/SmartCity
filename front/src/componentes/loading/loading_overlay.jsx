bca660f15e70126ae0ddb2ab7ff34687d75d256e
Lucas Camargo <114783200+LucasSilvaC@users.noreply.github.com>
2025-06-13 14:55:06 -0300

    Delete front directory

--- front/src/componentes/loading/loading_overlay.jsx
+++
@@ -1,9 +0,0 @@
-import { Atom } from "react-loading-indicators";
-
-export default function LoadingOverlay() {
-  return (
-    <div className="fixed inset-0 bg-black/30 backdrop-blur-sm  bg-opacity-60 z-50 flex items-center justify-center">
-      <Atom color="#00c476" size="medium" />
-    </div>
-  );
-}
--- front/src/pages/dashboards/dashboards.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Dashboards() {
-    return (
-        <>
-        <h1>oi</h1>
-        </>
-    )
-}
